aa80503d3224ef13ac1096dcf37eb5852d4d0909
Timothy O'Connell <toconnell@tyrannybelle.com>
2022-04-03 11:36:08 -0500

    0.7.34

--- v2/app/static/js/theLaborInVain.js
+++ v2/app/static/js/theLaborInVain.js
@@ -2,10 +2,23 @@
 
 var app = angular.module('theLaborInVain', ['ngAnimate']);
 
+// avoid clashes with jinja2
+app.config(['$interpolateProvider', function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{a');
+    $interpolateProvider.endSymbol('a}');
+}]);
+
 app.controller('rootScopeController', function($scope, $rootScope, $http) {
 
     $rootScope.init = function() {
+        $rootScope.curDate = new Date();
         console.info('Initialized in $rootScope!');
     };
     $rootScope.init();
+
+    $rootScope.loadUrl = function(destination) {
+       // allows us to use ng-click to re-direct to URLs
+        window.location = destination;
+    };
+
 });
--- v2/config.py
+++ v2/config.py
@@ -8,4 +8,4 @@ class Config():
 
     WS_DESC = 'A production company located in Chicago, IL USA.'
     WS_MOTTO = 'Bad news travels fast.'
-    VERSION = '0.2.19'
+    VERSION = '0.7.34'
